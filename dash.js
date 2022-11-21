0e26785e9daf649dd4fde4794e5af3ca23d3ba6b
Andrew Crawford <atcrawford18@gmail.com>
2022-11-21 10:26:48 -0500

    more changes 11/21

--- dash.js
+++ dash.js
@@ -1,4 +1,6 @@
 // Creating the SVG for the dashboard
+var width = 960;
+var height = 500;
 var svg2 = d3.select("body")
         .append("svg")
         .attr("width", width)
@@ -6,7 +8,62 @@ var svg2 = d3.select("body")
 
 //Write a function to make the dashboard 
 //Other things to do: read in data from different file--not state aggregated. Link each city to the state so that only cities in that state appear in when a state is selected. 
-function DashBoard(state){
+function DashBoard(state) {
+    var pathToCsv3 = "data/dns4.csv";
+    d3.dsv(",", pathToCsv3, function (d) {
+        return {
+            State_abbrev: d["State Abbreviation"],
+            StateName: d["State Name"],
+            CityName: d.City,
+            CountyName: d["County Name"],
+            ["Average 2020/2021 household income"]: d["Average Household Income 2020/2021"],
+            ["Historic Percent Change in Household Income 1984-2019"]: d["Percent Change in Household Income, 1984-2019"],
+            ["Average Yearly Historic Percent Change in Income 1984-2019"]: d["Average Yearly Percent Change in Household Income, 1984-2019"],
+            ["Minimum Wage"]: d["MinWage"],
+            ["Average House Price 2020/2021"]: d["Average House Price 2020/2021"],
+            ["Percent Change in Historic House Prices 2000-2019"]: d["Percent Change in Housing Prices, 2000-2019"],
+            ["Average Yearly Percent Change in Housing Prices 2000-2019"]: d["Average Yearly Percent Change in Housing Prices, 2000-2019"],
+            ["# of Violent Crime"]: d["Violent Crime Total"],
+            ["# of Property Crime"]: d["Property Crime Total"],
+            ["Property Crime Per Capita"]: d["Property Crime Per Capita"],
+            ["Violent Crime Per Capita"]: d["Violent Crime Per Capita"],
+            ["Total Crime Per Capita"]: d["Total Crime Per Capita"],
+            Pop: d.TotalPop,
+            Men: d.Men,
+            Women: d.Women,
+            VotingAge: d.VotingAgeCitizen,
+            Employed: d.Employed,
+            Hispanic: d.Hispanic,
+            White: d.White,
+            Black: d.Black,
+            Native: d.Native,
+            Asian: d.Asian,
+            Pacific: d.Pacific,
+            ["Average Income"]: d.Income,
+            ["Income Per Capita"]: d.IncomePerCap,
+            Poverty: d.Poverty,
+            ChildPoverty: d.ChildPoverty,
+            Professional: d.Professional,
+            Service: d.Service,
+            Office: d.Office,
+            Construction: d.Construction,
+            Production: d.Production,
+            Drive: d.Drive,
+            Carpool: d.Carpool,
+            Transit: d.Transit,
+            OtherTrans: d.OtherTransp,
+            WorkAtHome: d.WorkAtHome,
+            MeanCommute: d.MeanCommute,
+            PrivateWork: d.PrivateWork,
+            SelfEmployed: d.SelfEmployed,
+            FamilyWork: d.FamilyWork,
+            Unemployment: d.Unemployment,
+            Population: d.Population,
+
+        }
+    }).then(function (data) {
+
+    console.log(data);
     
     d3.select("#cityDropdown")
         .selectAll("options")
@@ -21,10 +78,11 @@ function DashBoard(state){
         });
        
             // event listener for the dropdown. Update choropleth and legend when selection changes. Call createMapAndLegend() with required arguments.
-        d3.select("#variableDropdown").on("change", function (d) {
+        d3.select("#cityDropdown").on("change", function (d) {
             svg.html("")
             createMap(json, data, d3.select(this).property("value"));
     });
         // run creation function on NumCities
         createMap(json, data, valuelist[0])
+    }
 }
