b5cfb3676a6b9275626bd86ee1786391e5b33269
Andrew Crawford <atcrawford18@gmail.com>
2022-11-22 22:28:36 -0500

    Added dash part to chory file

--- dash.js
+++ dash.js
@@ -1,18 +1,12 @@
 // Creating the SVG for the dashboard
-var width = 960;
-var height = 500;
-var svg2 = d3.select("body")
-        .append("svg")
-        .attr("width", width)
-        .attr("height", height);
 
 //Write a function to make the dashboard 
 //Other things to do: read in data from different file--not state aggregated. Link each city to the state so that only cities in that state appear in when a state is selected. 
 //Other things to do: initialize state variable in code for click in chory.js. then run function with that given state as the input. 
 // Write a function to make output once the city is selected from the dropdown. Another funciton may be needed to run the KNN as well. 
-function DashBoard(state) {
-    var pathToCsv3 = "data/dns4.csv";
-    d3.dsv(",", pathToCsv3, function (d) {
+function dashdrop(state){
+var pathToCsv3 = "data/dns4.csv";
+d3.dsv(",", pathToCsv3, function (d) {
         return {
             State_abbrev: d["State Abbreviation"],
             StateName: d["State Name"],
@@ -65,32 +59,50 @@ function DashBoard(state) {
         }
     }).then(function (data) {
 
-    console.log(data);
+        console.log(data);
         
-    data.forEach(function (d){
-        if (d.StateName === state){
-            "cityname" = d.CityName
-            }
         
-    })
     
-    d3.select("#cityDropdown")
-        .selectAll("options")
-        .data("cityname").enter()
-        .append("option")
-        .text(function (d) {
+        d3.select("#cDropdown")
+            .selectAll("options")
+            .append("option")
+            .text(function (d) {
             return d;
-        })
+            })
 //        .data(valuelist).enter()
-        .attr("value", function (d) {
-            return d;
-        });
+            //.attr("value", function (d) {
+            //return d;
+        //});
         
-     d3.select("#cityDropdown").on("change", function (d) {
-           svg2.html("")
+       // d3.select("#cityDropdown").on("change", function (d) {
+    let current_citiess = [];
+    let current_city_ids = 'current_cities_selection';
+    let current_city_idss = '#' + current_city_ids;
+            
+    
+        current_citiess = data.filter(function(d){ return d.StateName == state});
+        console.log(current_citiess)
+
+        d3.selectAll(current_city_ids).remove();
+        let selection = d3.select("#cDropdown")
+            .selectAll("options")
+            .data(current_citiess);
+        
+        selection.enter()
+            .append("option")
+            .attr('id', current_city_ids)
+            .merge(selection)
+            .text(function (d) {
+                return d.CityName;
+            });
+        
+})
+}
+    //console.log(#selection)
+   // updateCityDropdownD('Georgia');
 //Need to write another function for creating the table. Don't yet know what this table is supposed to look like. Also need to link to knn for the selected city. 
            //createMap(json, data, d3.select(this).property("value"));
-    });
-        // run creation function on NumCities
+    // run creation function on NumCities
   //      createMap(json, data, valuelist[0])
-    }
+    
+    
